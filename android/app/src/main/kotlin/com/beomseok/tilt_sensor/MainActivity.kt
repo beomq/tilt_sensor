cd18756577c800fe166d5d030e571786569494e8
beomq <beomdolq@gmail.com>
2023-06-18 01:56:43 +0900

    FEAT : tilt_sensor

---
+++ android/app/src/main/kotlin/com/beomseok/tilt_sensor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.beomseok.tilt_sensor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sensor_app.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
+class SensorApp extends StatelessWidget {
+  const SensorApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 화면 고정
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+    ]);
+    final centerX = MediaQuery.of(context).size.width / 2 - 50;
+    final centerY = MediaQuery.of(context).size.height / 2 - 50;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          StreamBuilder<AccelerometerEvent>(
+              stream: accelerometerEvents,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final event = snapshot.data!;
+                List<double> accelerometerValues = [event.x, event.y, event.z];
+                print(accelerometerValues);
+
+                return Positioned(
+                  left: centerX + event.y * 20,
+                  top: centerY + event.x * 20,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.green,
+                      shape: BoxShape.circle,
+                    ),
+                    width: 100,
+                    height: 100,
+                  ),
+                );
+              }),
+        ],
+      ),
+    );
+  }
+}
